e1db1af8d1829669be8dcc0c3c3c0f32087b89dd
Matt Ward <ward.matt@gmail.com>
2017-05-28 13:22:22 +0100

    Update addin version to 0.2

--- src/MonoDevelop.NuGetPackageExplorer/Properties/AddinInfo.cs
+++ src/MonoDevelop.NuGetPackageExplorer/Properties/AddinInfo.cs
@@ -29,7 +29,7 @@ using Mono.Addins;
 
 [assembly:Addin ("NuGetPackageExplorer",
                  Namespace = "MonoDevelop",
-                 Version = "0.1",
+                 Version = "0.2",
                  Category = "IDE extensions")]
 
 [assembly:AddinName ("NuGet Package Explorer")]
