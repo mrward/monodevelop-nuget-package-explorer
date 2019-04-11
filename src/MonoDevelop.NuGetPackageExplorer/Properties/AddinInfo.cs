0e3ad04fcfab30844999fb3865471a1d32608309
Matt Ward <ward.matt@gmail.com>
2019-04-11 18:31:41 +0100

    Update addin version to 0.3.1 to workaround addin server problem
    Addin server is publishing version 0.3 but for MonoDevelop 7.0. So
    update the version to 0.3.1 so the addin is available for 8.0.

--- src/MonoDevelop.NuGetPackageExplorer/Properties/AddinInfo.cs
+++ src/MonoDevelop.NuGetPackageExplorer/Properties/AddinInfo.cs
@@ -29,7 +29,7 @@ using Mono.Addins;
 
 [assembly:Addin ("NuGetPackageExplorer",
                  Namespace = "MonoDevelop",
-                 Version = "0.3",
+                 Version = "0.3.1",
                  Category = "IDE extensions")]
 
 [assembly:AddinName ("NuGet Package Explorer")]
